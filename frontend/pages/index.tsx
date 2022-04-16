3b3ccc2a06d022b10771c10c1658abccf9b18b22
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-04-16 12:24:18 +0545

    adding styked components

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+`;
+
+const Home = () => {
+  return (
+    <div>
+      <p>hello</p>
+      <Title>Title</Title>
+    </div>
+  );
+};
+
+export default Home;
